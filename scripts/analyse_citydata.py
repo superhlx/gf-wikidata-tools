13e240d8ea91e5c41d4d5cd2003ea6af809c316b
aarneranta <aarne@chalmers.se>
2025-02-05 13:38:08 +0100

    added BN and corrected JP to JA; printed some statistics

--- scripts/analyse_citydata.py
+++ scripts/analyse_citydata.py
@@ -9,29 +9,38 @@ DATA_FILE = 'City_Query_example.json'
 
 OUTFILE = 'City.jsonl'
 
-with open(DATA_FILE) as file:
-    data = json.load(file)
-
 def reorder(item):
     return {
         'url': item['city'],
         'labels': {
+            'bn': item.get('cityLabelBN', None),
             'en': item.get('cityLabelEN', None),
             'el': item.get('cityLabelEL', None),
-            'jp': item.get('cityLabelJP', None),
+            'ja': item.get('cityLabelJA', None),
             'sv': item.get('cityLabelSV', None),
             'zh': item.get('cityLabelZH', None)
             },
         'descriptions': {
+            'bn': item.get('descriptionBN', None),
             'en': item.get('descriptionEN', None),
             'el': item.get('descriptionEL', None),
-            'jp': item.get('descriptionJP', None),
+            'ja': item.get('descriptionJA', None),
             'sv': item.get('descriptionSV', None),
             'zh': item.get('descriptionZH', None)
             }
           }
-    
+
+with open(DATA_FILE) as file:
+    data = json.load(file)
+    data = [reorder(d) for d in data]
+    languages = data[0]['labels'].keys()
 
 for item in data:
-    print(json.dumps(reorder(item), ensure_ascii=False))
+    print(json.dumps(item, ensure_ascii=False))
 
+
+print("# Items:", len(data))
+print("# Labels:", {lang: len([
+        d for d in data if d['labels'][lang]]) for lang in languages})
+print("# Descriptions:", {lang: len([
+        d for d in data if d['descriptions'][lang]]) for lang in languages})
