8612fb1b403b1259a04b3e5c2956f60fd75bfad3
aarneranta <aarne@chalmers.se>
2025-03-07 14:48:57 +0100

    more robust mk_description.py

--- scripts/mk_description.py
+++ scripts/mk_description.py
@@ -1,17 +1,24 @@
+import sys
 import json
 import pgf
 
 # assumes gf -make Descriptions???.gf in ../grammars/
 
 GRAMMAR = '../grammars/Descriptions.pgf'
-DATA = 'Cities.json'
+
+DATA_FILE = 'Cities.json'
+if sys.argv[1:]:
+    DATA_FILE = sys.argv[1]
 
 BIG_POPULATION = 1000000
 
 grammar = pgf.readPGF(GRAMMAR)
 
-with open(DATA) as file:
-    data = json.load(file)
+with open(DATA_FILE) as file:
+    if DATA_FILE.endswith('.jsonl'):
+        data = [json.loads(line) for line in file]
+    else:
+        data = json.load(file)
 
 city_functions = {f.split('_')[0]: f for f in grammar.functions}
 
@@ -19,13 +26,19 @@ def city_description(country, property=None):
     if country:
         qid = country[len('http://www.wikidata.org/entity/'):]
         if fun := city_functions.get(qid, None):
-            if property:
-                return f'CityDescription ({property} city_Kind) (CountryLocation {fun})'
+            if property and fun:
+                fun = f'CityDescription ({property} city_Kind) (CountryLocation {fun})'
+            elif fun:
+                fun = f'CityDescription city_Kind (CountryLocation {fun})'
             else:
-                return f'CityDescription city_Kind (CountryLocation {fun})'
+                fun = 'city_Kind'
+            return pgf.readExpr(fun)
 
 def population(city):
-    return city.get('population', int(city['url'][-1])*200000)  # mock up
+    if p := city.get('population', None):
+        if p.isdigit():
+            return int(p)
+    return 0
 
 english = grammar.languages['DescriptionsEng']
 chinese = grammar.languages['DescriptionsChi']
@@ -35,12 +48,16 @@ swedish = grammar.languages['DescriptionsSwe']
 for city in data:
     country = city['country']
     if population(city) > BIG_POPULATION:
-        tree = pgf.readExpr(city_description(country, 'bigKind'))
+        tree = city_description(country, 'bigKind')
+    elif t := city_description(country):
+        tree = t
     else:
-        tree = pgf.readExpr(city_description(country))
-    print(city['labels']['en'], english.linearize(tree))
-    print(city['labels']['zh'], ''.join([c for c in chinese.linearize(tree) if not c.isspace()]))
-    print(city['labels']['sv'], swedish.linearize(tree))
-    print(city['labels']['el'], greek.linearize(tree))
+        tree = pgf.readExpr('city_Kind')
+    if tree is not None:
+        print(city['labels']['en'], tree)
+        print(city['labels']['en'], english.linearize(tree))
+        print(city['labels']['zh'], ''.join([c for c in chinese.linearize(tree) if not c.isspace()]))
+        print(city['labels']['sv'], swedish.linearize(tree))
+        print(city['labels']['el'], greek.linearize(tree))
 
 
