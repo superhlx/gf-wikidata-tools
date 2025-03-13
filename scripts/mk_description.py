a0f2b8968169ab6447420fdf8af196260463ee7c
Inari Listenmaa <inari.listenmaa@gmail.com>
2025-03-13 16:56:32 +0100

    check whether a city is capital, build GF tree with capitalKind if it is.
    NB. untested Python code, couldn't run it due to not having right JSON files available

--- scripts/mk_description.py
+++ scripts/mk_description.py
@@ -40,14 +40,19 @@ def population(city):
             return int(p)
     return 0
 
+def capital(city):
+    return bool(city.get('capital_of', False))
+
 english = grammar.languages['DescriptionsEng']
 chinese = grammar.languages['DescriptionsChi']
-greek = grammar.languages['DescriptionsGre']
-swedish = grammar.languages['DescriptionsSwe']
+#greek = grammar.languages['DescriptionsGre']
+#swedish = grammar.languages['DescriptionsSwe']
 
 for city in data:
     country = city['country']
-    if population(city) > BIG_POPULATION:
+    if capital(city):
+        tree = city_description(country, 'capitalKind')
+    elif population(city) > BIG_POPULATION:
         tree = city_description(country, 'bigKind')
     elif t := city_description(country):
         tree = t
